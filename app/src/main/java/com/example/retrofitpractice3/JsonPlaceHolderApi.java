03239a553419f099edddf7de07041a770bbfe868
chals001@naver.com <chldlftjr123>
2019-11-14 14:43:26 +0900

    commit all files

---
+++ app/src/main/java/com/example/retrofitpractice3/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package com.example.retrofitpractice3;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPost();
+}
